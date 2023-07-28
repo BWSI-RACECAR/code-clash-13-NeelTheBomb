53e09f9da7409c319011351e51bfe05892648a24
NeelTheBomb <77640484+NeelTheBomb@users.noreply.github.com>
2023-07-28 09:14:30 -0400

    Add files via upload

---
+++ stonks.py
@@ -0,0 +1,38 @@
+class Solution:
+    def stonks(self, prices):
+            #type prices: list of int
+            #return type: int
+            least = 0
+            least1 = 0
+            save = 0
+            save1=0
+            resultlist = 0
+            #TODO: Write code below to returnn an int with the solution to the prompt.
+            count = 0
+            for i in range(len(prices)):
+                for b in range(len(prices)):
+                    for c in range(len(prices)):
+                        least = 0
+                        save = b-i
+                        save1 = c-b
+                        resultlist.append(save+save1)
+            greatest = 0
+            for i in resultlist:
+                if i > greatest:
+                    graetest = i
+            return greatest
+                    
+
+            pass
+
+def main():
+    array = input().split(" ")
+    for x in range (0, len(array)):
+        array[x] = int(array[x])
+
+    tc1 = Solution()
+    ans = tc1.stonks(array)
+    print(ans)
+
+if __name__ == "__main__":
+    main()
